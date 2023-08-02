543d472f85b7e790838cf51a7b31c5053287fa5e
Ravi Chandra Padmala <neena@nilenso.com>
2023-08-02 17:23:33 +0530

    Add a rudimentary formula grammer
    Only a small fraction of the grammer is implemented. This is a work in progrss
    that will be expanded over the next couple weeks.

--- src/bean/core.cljs
+++ src/bean/core.cljs
@@ -1,7 +1,42 @@
 (ns bean.core
   (:require [instaparse.core :as insta]))
 
+(def ^:private parser (insta/parser
+              "
+              CellContents = UserExpression / Constant
+              Integer = #'[0-9]+'
+              String = #'.+'
+              Constant = Integer / String
 
+              UserExpression = '=' Expression
+              Expression = Value
+              Value = Integer / QuotedString
+              QuotedString = '\"' QuotedRawString '\"'
+              QuotedRawString = #'[^\"]+'
+              "))
 
-(def engine {})
+(defn parse [v]
+  (insta/parse parser v))
+
+(comment :grammer
+  "Constant = Integer | LiterallyAnythingString"
+  "CellContents = Constant | '=' Expression"
+
+  "CellAddress = CapitalLetter+ Number+"
+  "Operation = '+'"
+  "Integer = Number+"
+  "String = '"' Anything '"'"
+  "Value = Integer | String"
+  "Expression = CellAddress | Value  | Expression Operation Expression | FunctionInvocation"
+  "FunctionInvocation = FunctionName '(' FunctionArguments ')'"
+  "FunctionArguments = NULL | Expression | Expression ',' FunctionArguments"
+  )
+
+(comment :samples
+         "4"
+         "=4 + 9"
+         "foo"
+         "=C8"
+         "=concat('foo', C98)")
+
 
---
+++ test/bean_tests/core.cljs
@@ -0,0 +1,15 @@
+(ns bean-tests.core
+  (:require [bean.core :refer [parse]]
+            [clojure.test :refer :all]
+            [instaparse.core :as insta]))
+
+(deftest parser-test
+  (testing "Basic Parsing"
+      (is (= [:CellContents [:Constant [:Integer "4"]]]
+             (parse "4")))
+      (is (= [:CellContents [:Constant [:String "foo"]]]
+             (parse "foo")))
+      (is (= [:CellContents [:UserExpression "=" [:Expression [:Value [:Integer "89"]]]]]
+             (parse "=89")))
+      (is (= [:CellContents [:UserExpression "=" [:Expression [:Value [:QuotedString "\"" [:QuotedRawString "foo"] "\""]]]]]
+             (parse "=\"foo\"")))))
