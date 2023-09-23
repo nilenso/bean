262d0de880b0fd988f316120b9c806d39bccea44
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2023-09-23 20:07:52 +0530

    Spike absolute position and grid with 100k
    Grid doesn't seem to be considerably slower than absolute positioning
    with 50k elements with the benefit of merge etc.

--- src/bean/ui/main.cljs
+++ src/bean/ui/main.cljs
@@ -9,7 +9,7 @@
 
 (defn- start-sheet []
   (vec
-   (for [row-num (range 20)]
+   (for [row-num (range 300)]
      (vec (map (fn [_] "") (range 12))))))
 
 (defn update-cell [address content]
@@ -32,11 +32,81 @@
                         :set-mode set-mode
                         :edit-mode #(set-mode % :edit)}])
 
-(defn ^:export main []
-  (reset! sheet (grid/eval-sheet (start-sheet)))
+#_(defn ^:export main []
+  (reset! sheet {:grid (doall (for [i (range 200)]
+                                [{:content "", :ast [:CellContents], :value nil, :representation "cell1"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}
+                                 {:content "", :ast [:CellContents], :value nil, :representation "cell2"}]))})
   (r/render
    [:div
     [:div {:class :container}
      [cell-selector]
      [active-sheet]]]
    (.getElementById js/document "app")))
+
+
+#_(defn ^:export main []
+  (r/render
+   [:div {:id :container
+          :style {:display :grid 
+                  "grid-template-columns" (apply str (for [i (range 100)] (str (+ 50 (rand-int 100)) "px ")))
+                  :grid-template-rows "40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px"}}
+    [:div {:id "selector"}]
+    ;; 50000 takes 0.5s
+    ;; 100000 takes 1.2s
+    (for [i (range 50000)]
+      [:div 
+       {:style {:overflow :hidden
+                :border "0.25px solid #ddd"
+                :padding "5px"
+                :grid-column-start (when (= 1 (rand-int 20))
+                                     (str "span " (rand-int 10)))
+                :grid-row-start (when (= 1 (rand-int 20))
+                                  "span 3")
+                :background-color (when (= 1 (rand-int 50))
+                                    "beige")}}])]
+   (.getElementById js/document "app")))
+
+(defn ^:export main []
+    (r/render
+     [:div {:id :container
+            :style {:position :relative}}
+      [:div {:id "selector"}]
+      ;; 100000 takes 1.3s (without merge etc)
+      (for [i (range 100000)]
+        [:div
+         {:style {:position "absolute"
+                  :border "0.25px solid #ddd"
+                  :padding "5px"
+                  :top (str (* (/ i 100) 50) "px")
+                  :left (str (* (mod i 100) 100) "px")
+                  :background-color (when (= 1 (rand-int 50))
+                                      "beige")}} "HAHAHAHAHHA"])]
+     (.getElementById js/document "app")))
