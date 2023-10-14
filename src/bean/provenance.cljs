dc2042e27b9ca901eb56bd2b8ddf925c7baba239
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2023-10-14 12:19:55 +0530

    Add provenance for spilled cells

--- src/bean/provenance.cljs
+++ src/bean/provenance.cljs
@@ -5,13 +5,33 @@
 (defn- self-evident [value]
   [:value value :self-evident])
 
+(defn combine-op-proof [ast-result lproof op rproof]
+  (let [value (:value ast-result)
+        matrix (:matrix ast-result)
+        lmatrix (:matrix lproof)
+        rmatrix (:matrix rproof)]
+    (cond
+      (and lmatrix rmatrix) {:matrix
+                             (util/map-on-matrix-addressed
+                              #(do [:value (:value %2) (get-in lmatrix %1) op (get-in rmatrix %1)])
+                              matrix)}
+      lmatrix {:matrix
+               (util/map-on-matrix-addressed
+                #(do [:value (get-in matrix (conj %1 :value)) %2 op rproof])
+                lmatrix)}
+      rmatrix {:matrix (util/map-on-matrix-addressed
+                #(do [:value (get-in matrix (conj %1 :value)) lproof op %2])
+                rmatrix)}
+      :else [:value value lproof op rproof])))
+
 (declare cell-proof)
 
 (defn- ast-proof [ast grid]
   ;; ast goes down, proof come up
   (let [[node-type & [arg :as args]] ast
         sub-ast-proof #(ast-proof % grid)
-        ast-value #(:value (interpreter/eval-ast ast grid))]
+        ast-result #(interpreter/eval-ast ast grid)
+        ast-value #(:value (ast-result))]
     (case node-type
       :CellContents (if arg
                       (sub-ast-proof arg) [])
@@ -24,11 +44,11 @@
                        (util/map-on-matrix #(cell-proof % grid)))}
       :Expression (if (interpreter/is-expression? arg)
                     (let [[left op right] args]
-                      [:value
-                       (ast-value)
+                      (combine-op-proof
+                       (ast-result)
                        (sub-ast-proof left)
                        (sub-ast-proof op)
-                       (sub-ast-proof right)])
+                       (sub-ast-proof right)))
                     (sub-ast-proof arg))
       :Value (sub-ast-proof arg)
       :Integer (self-evident (ast-value))
@@ -38,14 +58,15 @@
 
 (defn- spilled-cell-proof [cell grid]
   (let [spilled-from (util/get-cell grid (:spilled-from cell))]
-    [:spill
-     {:spilled-from (:spilled-from cell)
-      :content (:content spilled-from)
-      :value (:value cell)
-      :relative-address (:relative-address cell)}
+    (conj
+     [:spill
+      {:spilled-from (:spilled-from cell)
+       :content (:content spilled-from)
+       :value (:value cell)
+       :relative-address (:relative-address cell)}]
      (get-in
       (:matrix (ast-proof (:ast spilled-from) grid))
-      (:relative-address cell))]))
+      (:relative-address cell)))))
 
 (defn cell-proof
   "Returns a hiccup style proof tree.
--- test/bean_tests/provenance.cljs
+++ test/bean_tests/provenance.cljs
@@ -97,6 +97,67 @@
                :relative-address [1 0]}
               [:cell-ref
                {:address [1 0] :content "2" :value 2}
-               [:value 2 :self-evident]]])))))
+               [:value 2 :self-evident]]]))))
+
+  (testing "Provenance for matrix opertions"
+    (let [evaled-grid (grid/eval-sheet [["1" "=A1:A2+C1:C2" "3"]
+                                        ["2" "" "=A1"]])]
+      (is (= (provenance/cell-proof [1 1] (:grid evaled-grid))
+             [:spill
+              {:spilled-from [0 1]
+               :content "=A1:A2+C1:C2"
+               :value 3
+               :relative-address [1 0]}
+              [:value 3
+               [:cell-ref
+                {:address [1 0] :content "2" :value 2}
+                [:value 2 :self-evident]]
+               [:value "+" :self-evident]
+               [:cell-ref
+                {:address [1 2] :content "=A1" :value 1}
+                [:cell-ref
+                 {:address [0 0] :content "1" :value 1}
+                 [:value 1 :self-evident]]]]]))))
+
+  (testing "Provenance for matrix-scalar operations"
+    (let [evaled-grid (grid/eval-sheet [["1" "=A1:A2+1"]
+                                        ["2" ""]])]
+      (is (= (provenance/cell-proof [1 1] (:grid evaled-grid))
+             [:spill
+              {:spilled-from [0 1]
+               :content "=A1:A2+1"
+               :value 3
+               :relative-address [1 0]}
+              [:value 3
+               [:cell-ref
+                {:address [1 0] :content "2" :value 2}
+                [:value 2 :self-evident]]
+               [:value "+" :self-evident]
+               [:value 1 :self-evident]]])))
+
+    (let [evaled-grid (grid/eval-sheet [["1" "=A1:A2+C1:C2+D1:D2+1" "3" "0"]
+                                        ["2" "" "4" "0"]])]
+      (is (= (provenance/cell-proof [1 1] (:grid evaled-grid))
+             [:spill
+              {:spilled-from [0 1]
+               :content "=A1:A2+C1:C2+D1:D2+1"
+               :value 7
+               :relative-address [1 0]}
+              [:value 7
+               [:cell-ref
+                {:address [1 0] :content "2" :value 2}
+                [:value 2 :self-evident]]
+               [:value "+" :self-evident]
+               [:value 5
+                [:cell-ref
+                 {:address [1 2] :content "4" :value 4}
+                 [:value 4 :self-evident]]
+                [:value "+" :self-evident]
+                [:value 1
+                 [:cell-ref
+                  {:address [1 3] :content "0" :value 0}
+                  [:value 0 :self-evident]]
+                 [:value "+" :self-evident]
+                 [:value 1 :self-evident]]]]])))))
 
 (run-all-tests)
