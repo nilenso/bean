ae0bbb5054db6a19325cdacf83098c8337959b10
Ravi Chandra Padmala <neena@nilenso.com>
2023-11-09 15:35:29 -0500

    Tie in secondary space UI with the functionality

---
+++ src/bean/scratch.cljs
@@ -0,0 +1,11 @@
+(ns bean.scratch
+  (:require [bean.grid :as grid]))
+
+(defn reevaluate [{:keys [code-in-editor] :as sheet}]
+  (grid/eval-code sheet code-in-editor))
+
+(defn set-code [sheet code]
+  (assoc sheet :code-in-editor code))
+
+(defn get-code [sheet]
+  (get sheet :code-in-editor))
---
+++ src/bean/ui/scratch.cljs
@@ -0,0 +1,38 @@
+(ns bean.ui.scratch
+  (:require [bean.scratch :as scratch]
+            [bean.ui.util :refer [cs]]))
+
+(defn set-eval-state [sheet]
+  (assoc-in sheet
+            [:ui :scratch-evaluation-state]
+            (if (:errors sheet)
+              :error
+              :evaluated)))
+
+(defn set-eval-state-pending [sheet]
+  (assoc-in sheet [:ui :scratch-evaluation-state] :pending))
+
+(defn text-area [sheet]
+  [:div {:class (cs (str "scratch-state-"
+                         (name (or (get-in @sheet [:ui :scratch-evaluation-state])
+                                   :evaluated)))
+                    :scratch)}
+   [:div {:class "scratch-header bean-label"}
+    [:button {:on-click (fn [_]
+                          (swap! sheet #(-> %
+                                            scratch/reevaluate
+                                            set-eval-state)))}
+     "▶"]]
+   [:div {:class :scratch-thick-lines}]
+   [:div {:class :scratch-body}
+    [:div {:class :scratch-margin}]
+    [:textarea
+      ;; TODO: The textarea and the scratch should keep expanding as more text is added
+     {:class :scratch-text
+      :content-editable ""
+      :on-change (fn [e]
+                   (swap! sheet #(-> %
+                                     (scratch/set-code (.-value (.-target e)))
+                                     set-eval-state-pending)))
+      :spell-check false
+      :default-value (scratch/get-code @sheet)}]]])
