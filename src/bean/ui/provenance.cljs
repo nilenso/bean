45d64eacf4d4c603f76e4f499126af5e3b0bcb3c
Ravi Chandra Padmala <neena@nilenso.com>
2023-11-27 14:03:34 -0500

    Fix provenance ui integration
    This was broken in the move from :value -> :scalar. Wasn't caught since
    we don't have UI tests.

--- src/bean/ui/provenance.cljs
+++ src/bean/ui/provenance.cljs
@@ -11,14 +11,14 @@
          (fn [[s queue] subproof]
            (let [[proof-type p & rest] subproof]
              [(str s (case proof-type
-                       :value (str p " ")
+                       :scalar (str p " ")
                        :spill (str (apply util/rc->a1 (:address p)) " ")
                        :cell-ref (str (apply util/rc->a1 (:address p)) " ")))
               (if (coll? (first rest))
                 (conj queue subproof)
                 queue)]))
          (case _
-           :value [(str proof " is ") []]
+           :scalar [(str proof " is ") []]
            :spill [(str (apply util/rc->a1 (:address proof)) " is ") []]
            :cell-ref [(str (apply util/rc->a1 (:address proof)) " is ") []])
          dependency-proofs)]
@@ -27,16 +27,16 @@
         (str res "\n" new-spacer "where " (string/join (str  "\n" new-spacer "and ") (map #(subtree-proof-sentence new-spacer %) que))))
       res)))
 
-(defn sentence-proof 
+(defn sentence-proof
   ([expression sheet]
    (let [proof-tree (provenance/ast-proof
                      (parser/parse (str "=" expression))
                      sheet)
          [proof-type proof & dependency-proofs] proof-tree
          sproof (case proof-type
-                  :value (str "it's " proof)
-                  :spill (str "it's " (:value proof))
-                  :cell-ref (str (apply util/rc->a1 (:address proof)) " is " (:value proof)))]
+                  :scalar (str "it's " proof)
+                  :spill (str "it's " (:scalar proof))
+                  :cell-ref (str (apply util/rc->a1 (:address proof)) " is " (:scalar proof)))]
      (if (coll? (first dependency-proofs))
        (str sproof " because\n" (subtree-proof-sentence "" proof-tree))
        sproof))))
